fd0fa6b5e7e0ea3e250769778317b1f9c15f85c3
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2021-11-24 18:29:23 +0800

    Update ajaxfile.php

--- ajaxfile.php
+++ ajaxfile.php
@@ -1,34 +1,29 @@
 <?php
+
 include 'config.php';
 
 ## Read value
-$page  = $_GET['page'];
-$size  = $_GET['size']; // Rows display per page
-
-
-
-
+$page = $_GET['page'];
+$size = $_GET['size']; // Rows display per page
 ## Total number of records without filtering
-$sel = mysqli_query($con,"select count(*) as allcount from employee");
+$sel = mysqli_query($con, "select count(*) as allcount from employee");
 $records = mysqli_fetch_assoc($sel);
 $totalRecords = $records['allcount'];
-$last_page= ceil($totalRecords/$size);
-
-
+$last_page = ceil($totalRecords / $size);
 
 ## Fetch records
-$empQuery = "select * from employee limit ".($page-1)*$size.",".$size;
+$empQuery = "select * from employee limit " . ($page - 1) * $size . "," . $size;
 $empRecords = mysqli_query($con, $empQuery);
 $data = array();
 
 while ($row = mysqli_fetch_assoc($empRecords)) {
     $data[] = array(
-    		"emp_name"=>$row['emp_name'],
-    		"email"=>$row['email'],
-    		"gender"=>$row['gender'],
-    		"salary"=>$row['salary'],
-    		"city"=>$row['city']
-    	);
+        "emp_name" => $row['emp_name'],
+        "email" => $row['email'],
+        "gender" => $row['gender'],
+        "salary" => $row['salary'],
+        "city" => $row['city']
+    );
 }
 
 ## Response
