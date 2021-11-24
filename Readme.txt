a4e41fdd56cea9d3cee53eaea5c5ca0715d31c68
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2021-11-24 18:27:35 +0800

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+Instructions
+1. Import employee.sql table in your MySQL database.
+2. Update config.php file.
+
---
+++ ajaxfile.php
@@ -0,0 +1,40 @@
+<?php
+include 'config.php';
+
+## Read value
+$page  = $_GET['page'];
+$size  = $_GET['size']; // Rows display per page
+
+
+
+
+## Total number of records without filtering
+$sel = mysqli_query($con,"select count(*) as allcount from employee");
+$records = mysqli_fetch_assoc($sel);
+$totalRecords = $records['allcount'];
+$last_page= ceil($totalRecords/$size);
+
+
+
+## Fetch records
+$empQuery = "select * from employee limit ".($page-1)*$size.",".$size;
+$empRecords = mysqli_query($con, $empQuery);
+$data = array();
+
+while ($row = mysqli_fetch_assoc($empRecords)) {
+    $data[] = array(
+    		"emp_name"=>$row['emp_name'],
+    		"email"=>$row['email'],
+    		"gender"=>$row['gender'],
+    		"salary"=>$row['salary'],
+    		"city"=>$row['city']
+    	);
+}
+
+## Response
+$response = array(
+    "last_page" => $last_page,
+    "data" => $data
+);
+
+echo json_encode($response);
